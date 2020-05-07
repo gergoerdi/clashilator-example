d5356e2cfe29bedfb1f10eb1875b56a66f190b09
Dr. ERDI Gergo <gergo@erdi.hu>
2020-05-07 19:55:46 +0800

    Use `clashilatorMain` to simplify `Setup.hs`

--- Setup.hs
+++ Setup.hs
@@ -1,16 +1,4 @@
-import Clash.Clashilator.Setup (clashilate)
-
-import Distribution.Simple
-import Distribution.Simple.LocalBuildInfo
-import Distribution.Simple.Setup
-import Distribution.PackageDescription
+import Clash.Clashilator.Setup (clashilatorMain)
 
 main :: IO ()
-main = defaultMainWithHooks simpleUserHooks
-    { buildHook = myBuildHook
-    }
-
-myBuildHook :: PackageDescription -> LocalBuildInfo -> UserHooks -> BuildFlags -> IO ()
-myBuildHook pkg localInfo userHooks buildFlags = do
-    pkg' <- clashilate pkg localInfo buildFlags
-    buildHook simpleUserHooks pkg' localInfo userHooks buildFlags
+main = clashilatorMain
