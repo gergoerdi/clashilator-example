59b60b90cf5231a47bb6d63c94c4c216cb270c87
Dr. ERDI Gergo <gergo@erdi.hu>
2020-05-07 12:15:49 +0800

    Add a gratuitous dependency on MTL, just to show that Clash code can use other packages via Cabal

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -1,6 +1,7 @@
 module MyLib (mainBoard) where
 
 import Clash.Prelude
+import Control.Monad.Reader
 
 mainBoard :: Signal System Bit -> Signal System Bit
-mainBoard = fmap complement
+mainBoard = fmap $ asks complement
